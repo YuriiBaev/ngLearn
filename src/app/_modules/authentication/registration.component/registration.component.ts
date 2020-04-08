c5e10e008b33d26042133b28ba234cf928f0bff9
Yuriy Baiev <yuriy.baev@nixsolutions.com>
2020-04-08 11:52:21 +0300

    Rework validation

--- src/app/_modules/authentication/registration.component/registration.component.ts
+++ src/app/_modules/authentication/registration.component/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '@services/auth-service/auth.service';
 
 @Component({
@@ -13,15 +13,51 @@ export class RegistrationComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.registrationForm = this.formBuilder.group({
       name: ['', Validators.required],
       surname: ['', Validators.required],
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(6)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, this.confirmPasswordValidator]],
     });
+
+    this.registrationForm.valueChanges.subscribe(({password}) => {
+      const confirmationPasswordControl = this.getFormControl('confirmPassword');
+
+      if (password && confirmationPasswordControl.disabled) {
+        confirmationPasswordControl.enable();
+      }
+    });
+  }
+
+  getFormControl(name: string) {
+    return this.registrationForm && this.registrationForm.controls[name];
+  }
+
+  getFormValue(name: string) {
+    return this.registrationForm && this.registrationForm.value[name];
+  }
+
+  confirmPasswordValidator = ({value}: AbstractControl) => {
+    if (!this.registrationForm) { return null; }
+
+    const password = this.getFormValue('password');
+
+    if (!password) {
+      this.getFormControl('confirmPassword').disable();
+
+      return null;
+    }
+
+    if (password !== value) {
+      return {passwordMismatch: true};
+    } else {
+      return null;
+    }
   }
 
   onSubmit() {
--- src/app/_services/forms-validation/forms-validation.service.ts
+++ src/app/_services/forms-validation/forms-validation.service.ts
@@ -24,7 +24,27 @@ export class FormsValidationService {
     this.serverErrors$.next(null);
   }
 
-  validate = (form: FormGroup, fieldName: string) => (
-    form.controls[fieldName].invalid && form.controls[fieldName].touched
-  )
+  validate = (form: FormGroup, fieldName: string) => {
+    const formControl = form.controls[fieldName];
+    if (formControl.invalid && formControl.touched && formControl.errors) {
+      return this.getErrorMessage(formControl.errors);
+    }
+    return false;
+  }
+
+  getErrorMessage = (errors) => {
+    const firstError = Object.keys(errors)[0];
+
+    switch (firstError) {
+      case 'required':
+        return 'This field is required';
+      case 'minlength':
+        return 'Length of password should be more than 6 symbols';
+      case 'passwordMismatch':
+        return 'Check your confirmation password it should be the same as password';
+      default:
+          return firstError;
+
+    }
+  }
 }
